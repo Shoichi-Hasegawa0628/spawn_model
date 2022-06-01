da16d642830b5693cea315e24f2a7b12304c0b37
sakaguchi.taichi <sakaguchi.taichi@em.ci.ritsumei.ac.jp>
2022-06-01 17:29:32 +0900

    change object kind and place

--- src/__init__.py
+++ src/__init__.py
@@ -2,63 +2,48 @@
 # -*- coding: utf-8 -*-
 
 ## Object file path ##
-PATH = "frontiers2021_gazebo_worlds"
+# PATH = "frontiers2021_gazebo_worlds"
+PATH = "tmc_gazebo_worlds"
 
 ###################################################
 ## Object file name
 # tableware
-plate = "ycb_029_plate"
-bowl = "ir_bowl"
-pitcher_base = "ycb_019_pitcher_base"
 
 # fruits
 banana = "ycb_011_banana"
 apple = "ycb_013_apple"
 orange = "ycb_017_orange"
 
-# snack
-cracker_box = "ycb_003_cracker_box"
-pudding_box = "ycb_008_pudding_box"
-chips_bag = "nrp_7eleven_bag_chips_consomme"
-
 # bottle
 coffee = "nrp_7eleven_bottle_coffee"
 muscat = "nrp_7eleven_bottle_juice_grape"
 fruits_juice = "nrp_7eleven_bottle_juice_tropical"
 
-# doll
-penguin_doll = "3rd_generic_toy_doll_penguin"
-sheep_doll = "3rd_generic_toy_doll_sheep"
-pig_doll = "3rd_generic_toy_doll_pig"
-
-# vehicle_toy
-airplane_toy = "ycb_072-a_toy_airplane"
-car_toy = "ycb_073-g_lego_duplo"
-truck_toy = "ir_truck"
-
 # toiletries
-tooth_paste = "ir_tooth_paste"
-towel = "ir_towel"
-cup = "ir_cup"
+cup = "ycb_025_mug"
 
-# bath_goods
-treatments = "ir_treatments"
-body_sponge = "ycb_026_sponge"
-bath_slipper = "ir_bath_slipper"
+# snacks
+pudding_box = "ycb_008_pudding_box"
+chips_bag = "nrp_7eleven_bag_chips_consomme"
+
+
 
 ###################################################
 ## Object list
-objects = [plate, bowl, pitcher_base, banana, apple,
-           orange, cracker_box, pudding_box, chips_bag,
-           coffee, muscat, fruits_juice, penguin_doll,
-           sheep_doll, pig_doll, airplane_toy, car_toy,
-           truck_toy, tooth_paste, towel, cup,
-           treatments, body_sponge, bath_slipper]
+objects = [banana, apple, orange, pudding_box, chips_bag,
+           coffee, muscat, fruits_juice, cup]
 
 ###################################################
 # Target place name, position.
-living = [-0.114105, -0.078607, 3.0, -0.000759, 0.000129, 0.133706]
-kitchen = [0.875938, -2.357945, 3.0, -0.000760, -0.000233, -0.296452]
-bedroom = [-2.760358, -0.389499, 3.0, -0.000128, -0.000918, -1.496207]
-bathroom = [-1.062587, 3.753711, 3.0, 0.000826, -0.000479, -2.729761]
-places = [living, kitchen, bedroom, bathroom]
+banana_place = [1.187147, -0.196535, 0.400285, -0.004988, -0.008848, -1.435205]
+apple_place = [1.224642, 0.144890, 0.400071, 0.026494, 0.026494, -0.032608, 0.001624]
+orange_place = [1.207676, 0.505466, 0.400118, 0.004569, 0.013818, 0.000066]
+pudding_box_place = [1.169954, 1.160326, 0.627735, 1.560782, 0.486799, -1.720285]
+coffee_place = [-0.998882, 0.901094, 0.705108, 0.00, 0.00, 0.00]
+fruits_juice_place = [-1.055257, 0.716371, 0.429611, 0.00, 0.00, 0.00]
+cup_palce = [-1.036964, -0.145013, 0.400506, -0.001387, -0.004678, -0.000219]
+chips_bag_place = [-1.083096, -0.469417, 0.419209, 0.00, 0.00, 0.00]
+muscat_place = [-1.106978, -0.734402, 0.405169, 0.00, 0.00, 0.00]
+
+places = [banana_place, apple_place, orange_place, pudding_box_place, chips_bag_place,
+          coffee_place, muscat_place, fruits_juice_place, cup_palce]
